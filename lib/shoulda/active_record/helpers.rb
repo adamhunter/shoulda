6f0ed1a4988c69d28f604747c61ae82eb78bbd16
Adam Hunter <adamhunter@tma1.com>
2010-02-25 13:04:09 -0500

    updated error matching for rails3

--- lib/shoulda/active_record/helpers.rb
+++ lib/shoulda/active_record/helpers.rb
@@ -16,7 +16,9 @@ module Shoulda # :nodoc:
       #   default_error_message(:too_short, :count => 5)
       #   default_error_message(:too_long, :count => 60)
       def default_error_message(key, values = {})
-        if Object.const_defined?(:I18n) # Rails >= 2.2
+        if Object.const_defined?(:ActiveModel)
+          I18n.translate("errors.messages.#{key}", values)
+        elsif Object.const_defined?(:I18n) # Rails >= 2.2
           I18n.translate("activerecord.errors.messages.#{key}", values)
         else # Rails <= 2.1.x
           ::ActiveRecord::Errors.default_error_messages[key] % values[:count]
