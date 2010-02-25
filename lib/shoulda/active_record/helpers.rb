37e317c7cc75708ee1949fe2e1f4a66e034c4887
Adam Hunter <adamhunter@tma1.com>
2010-02-25 13:11:53 -0500

    splitting error logic for activemodel and activerecord

--- lib/shoulda/active_record/helpers.rb
+++ lib/shoulda/active_record/helpers.rb
@@ -17,7 +17,13 @@ module Shoulda # :nodoc:
       #   default_error_message(:too_long, :count => 60)
       def default_error_message(key, values = {})
         if Object.const_defined?(:ActiveModel)
-          I18n.translate("errors.messages.#{key}", values)
+          translation = I18n.translate("errors.messages.#{key}", values)
+          if translation =~ /translation missing/
+            # change to use any orm
+            I18n.translate("activerecord.errors.messages.#{key}", values)
+          else
+            translation
+          end
           # remove below?
         elsif Object.const_defined?(:I18n) # Rails >= 2.2
           I18n.translate("activerecord.errors.messages.#{key}", values)
